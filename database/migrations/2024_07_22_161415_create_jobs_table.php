cf3f47a74cb5467b5f369f37c3f427b64b85a0e9
kikna <mister.kiknadze@gmail.com>
2024-07-22 20:34:50 +0400

    chore: Add company, jobs, and applications migrations

---
+++ database/migrations/2024_07_22_161415_create_jobs_table.php
@@ -0,0 +1,44 @@
+
+
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+
+
+
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('company');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+            $table->boolean('isactive');
+            $table->boolean('is_resume_required');
+            $table->date('start_date');
+            $table->date('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
