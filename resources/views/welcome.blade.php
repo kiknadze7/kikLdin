8cf9b5855a92fc43264a37a38f70633067ba89fe
kikna <mister.kiknadze@gmail.com>
2024-07-23 21:14:53 +0400

    chore: Update welcome.blade.php to add Swiper carousel for companies and grid layout for jobs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,39 +3,50 @@
 @section('title', 'Main Page')
 
 @section('content')
-    <div class="swiper mySwiperClass" style='
-    width:90% ;
-    height:230px
-    '>
+    <div class="swiper mySwiperClass" style='width:90%; height:230px;'>
         <div class="swiper-wrapper">
-            <!-- Slides -->
 
+            <!-- Slides -->
             @for ($i = 0; $i < 10; $i++)
                 <div class="swiper-slide">
-                    <div class="card ">
+                    <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title
-                            ">Company {{ $i }}</h5>
+                            <h5 class="card-title">Company {{ $i }}</h5>
                             <div style="display:flex">
                                 <p class="card-text">
                                     DescriptionDescriptionDescription
                                     DescriptionDescriptionDescriptionDescri
                                     ptionDescriptionDescriptionDescription
                                     ptionDescriptionDescriptionDescription
-                                    {{ $i }}</p>
-                                <img style="
-                                width:100px;
-                                height:100px;"
-                                    src="https://via.placeholder.com/150" class="card-img-top"
-                                    alt="Company {{ $i }}" />
+                                    {{ $i }}
+                                </p>
+                                <img style="width:100px; height:100px;" src="https://via.placeholder.com/150"
+                                    class="card-img-top" alt="Company {{ $i }}" />
                             </div>
                         </div>
                     </div>
                 </div>
             @endfor
-
         </div>
         <div class="swiper-pagination"></div>
+    </div>
+
+    <!-- Grid Job Cart -->
+    <div class="container mt-5">
+        <div class="row">
+            @for ($j = 0; $j < 26; $j++)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Job {{ $j }}</h5>
+                            <p class="card-text">
+                                Job Description {{ $j }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endfor
+        </div>
     </div>
 @endsection
 
